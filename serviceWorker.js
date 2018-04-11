5cb43f4c0bce5ee249560c4c0840c19375004687
Max Frederiksen <max.frederiksencoles@gmail.com>
2018-04-11 22:23:38 +0200

    First commit of mws restaurant application stage 1

---
+++ serviceWorker.js
@@ -0,0 +1,58 @@
+const CACHES_NAME = 'assets-v1';
+const CACHES = [
+	'/index.html',
+	'/js/main.js',
+	'/css/styles.css',
+];
+
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  	.register('/serviceWorker.js')
+  	.then(function(sw) { 
+  		console.log("Service Worker Registered");
+  	});
+}
+
+self.addEventListener('install', function(e) {
+	console.log('installing..');
+	 e.waitUntil(
+	   caches.open(CACHES_NAME).then(function(cache) {
+	     return cache.addAll(CACHES);
+	   })
+	 );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+      	
+      	console.log(response);
+
+        if (response) {
+          return response;
+        }
+
+        let fetchClone = event.request.clone();
+
+        return fetch(fetchClone).then(
+          function(response) {
+
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            let responseToCache = response.clone();
+
+            caches.open(CACHES_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
